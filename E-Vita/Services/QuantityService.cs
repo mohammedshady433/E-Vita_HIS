36cf2ba185a90aa6a8d5a695dcfdc09ed2c66a6e
khaled ahmed <khaled95.ahmed95@gmail.com>
2025-05-17 00:54:22 +0300

    bebo

---
+++ E-Vita/Services/QuantityService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Vita_APIs.Models;
+
+namespace E_Vita.Services
+{
+    public class QuantityService : BaseApiService
+    {
+
+        public async Task<List<Quantity>> GetAllAsync()
+        {
+            return await GetAsync<List<Quantity>>(Quantity);
+        }
+
+        public async Task<Quantity> GetByIdAsync(int id)
+        {
+            return await GetAsync<Quantity>($"{Quantity}/{id}");
+        }
+
+        public async Task<bool> AddAsync(Quantity quantity)
+        {
+            return await PostAsync(Quantity, quantity);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await DeleteAsync($"{Quantity}/{id}");
+        }
+    }
+}
---
+++ E-Vita/Services/RoomService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Vita.Services
+{
+    public class RoomService : BaseApiService
+    {
+
+        public async Task<List<Room>> GetAllAsync()
+        {
+            return await GetAsync<List<Room>>(Room);
+        }
+
+        public async Task<Room> GetByIdAsync(int id)
+        {
+            return await GetAsync<Room>($"{Room}/{id}");
+        }
+
+        public async Task<bool> AddAsync(Room room)
+        {
+            return await PostAsync(Room, room);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await DeleteAsync($"{Room}/{id}");
+        }
+
+        public async Task<bool> UpdateAsync(int id, Room updatedRoom)
+        {
+            return await PutAsync($"{Room}/{id}", updatedRoom);
+        }
+    }
+}
