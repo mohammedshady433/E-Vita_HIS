61eadbde7da10eaea7c1af419e03cf5d2a2e0495
Mohammed Shady <154020799+mohammedshady433@users.noreply.github.com>
2025-05-16 21:25:23 +0300

    BaseClass of the API services

---
+++ E-Vita/Services/BaseApiService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using E_Vita.Models;
+
+namespace E_Vita.Services
+{
+    public class BaseApiService
+    {
+        protected readonly HttpClient _httpClient;
+        public BaseApiService()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7037/api/")
+            };
+        }
+
+        protected async Task<T> GetAsync<T>(string endpoint)
+        {
+            var response = await _httpClient.GetAsync(endpoint);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                return JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive=true});
+            }
+            return default;
+        }
+
+        protected async Task<bool> PostAsync<T>(string endpoint, T data)
+        {
+            var json = JsonSerializer.Serialize(data);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(endpoint, content);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
