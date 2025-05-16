b4d09d40f3961001627685e4ff8d2d73a734a32a
shahd mostafa <shahdmostafaa98@gmail.com>
2025-05-17 01:24:02 +0300

    3 services are done

---
+++ E-Vita/Services/ScheduleServices.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using E_Vita_APIs.Models; 
+
+namespace E_Vita.Services
+{
+    internal class ScheduleServices : BaseApiService
+    {
+        private const string endpoint = "schedle";
+
+        public async Task<List<Scheduale>> GetAllAsync()
+        {
+            return await GetAsync<List<Scheduale>>(endpoint);
+        }
+
+        public async Task<Scheduale> GetByIdAsync(int id)
+        {
+            return await GetAsync<Scheduale>($"{endpoint}/{id}");
+        }
+
+        public async Task<bool> AddAsync(Scheduale scheduale)
+        {
+            return await PostAsync(endpoint, scheduale);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await base.DeleteAsync($"{endpoint}/{id}");
+        }
+    }
+}
+
+
---
+++ E-Vita/Services/WardroundServices.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using E_Vita_APIs.Models; 
+namespace E_Vita.Services
+{
+    internal class WardroundServices : BaseApiService
+    {
+        private const string endpoint = "wardround"; 
+
+        
+        public async Task<List<WardRound>> GetAllAsync()
+        {
+            return await GetAsync<List<WardRound>>(endpoint);
+        }
+
+        
+        public async Task<WardRound> GetByIdAsync(int id)
+        {
+            return await GetAsync<WardRound>($"{endpoint}/{id}");
+        }
+
+        
+        public async Task<bool> AddAsync(WardRound wardRound)
+        {
+            return await PostAsync(endpoint, wardRound);
+        }
+
+        
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await base.DeleteAsync($"{endpoint}/{id}");
+        }
+    }
+}
+
